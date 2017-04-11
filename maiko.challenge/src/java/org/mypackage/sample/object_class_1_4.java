6114dc80e4e9c7ee099fa990b7f63f9d29c5794d
maiko saito <smoacickeor@yahoo.co.jp>
2017-04-11 17:48:55 +0900

    Java・オブジェクト指向1-4の課題を提出致します。

---
+++ maiko.challenge/src/java/org/mypackage/sample/object_class_1_4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+/**
+ *
+ * @author maiko
+ */
+public class object_class_1_4 extends object_class_1_3 {
+//クラスを継承
+    
+public void noname(){
+    // 継承した変数を初期化
+    String firstName = null;
+    String lastName = "";
+}
+
+public static void main(String[] args){
+    
+        object_class_1_3 person3 = new object_class_1_3();{
+        System.out.print(person3.getFullName());
+}
+
+}
+}
